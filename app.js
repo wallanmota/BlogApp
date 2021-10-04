882eed3f35aafa54add1623c3c030679b123215a
wallanmota <wallanmota.13@gmail.com>
2021-10-04 17:07:31 -0300

    ajustes para deploy

--- app.js
+++ app.js
@@ -121,7 +121,7 @@
     app.use('/usuarios', usuarios)
 
 //Outros
-    const PORT = 5000
+    const PORT = process.env.PORT || 5000
     app.listen(PORT, ()=>{
         console.log('Servidor rodando!')
     });
